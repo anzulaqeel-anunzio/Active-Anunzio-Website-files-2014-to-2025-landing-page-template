85bb7d918121201cccfd3067c064cac4d8658a73
Anzul Aqeel <anzul.aa@gmail.com>
2025-12-28 15:21:46 +0400

    Content update with README

---
+++ active/js/scripts.js
@@ -0,0 +1,71 @@
+﻿/*
+	Active by TEMPLATE <a href="https://anunziointernational.com/software/all-web-applications-by-anunzio.html" target="_blank">Anzul Aqeel</a>
+	templatestock.co @templatestock
+	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+*/
+
+
+jQuery(function($){
+
+'use strict';
+
+
+    /* ---------------------------------------------- /*
+     * Preloader
+    /* ---------------------------------------------- */
+	(function () {
+	    $(window).load(function() {
+	        $('#pre-status').fadeOut();
+	        $('#st-preloader').delay(350).fadeOut('slow');
+	    });
+	}());
+
+
+
+    /* ---------------------------------------------- /*
+     * Animated scrolling / Scroll Up
+    /* ---------------------------------------------- */
+    (function () {
+        $('a[href*=#]').bind("click", function(e){
+            var anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $(anchor.attr('href')).offset().top
+            }, 1000);
+            e.preventDefault();
+        });
+    }());
+
+
+    /* ---------------------------------------------- /*
+     * Full Screen
+    /* ---------------------------------------------- */
+    (function () {
+        $(".st-fullHeight").height($(window).height());
+
+        $(window).resize(function(){
+            $(".st-fullHeight").height($(window).height());
+        });
+
+    }());
+	
+/* Developed by Anunzio International */
+
+    /* ---------------------------------------------- /*
+     * Parallax Scrolling
+    /* ---------------------------------------------- */
+	(function () {
+		$(window).bind('load', function () {
+			parallaxInit();						  
+		});
+		function parallaxInit() {		
+			$(".subscribe").parallax("50%", 0.3);
+		}	
+		parallaxInit();
+	}());
+
+
+		
+
+	
+
+});
